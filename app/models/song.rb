3a9259c8a2c1153b834360a43076f0960ea48413
aaron-schuyler <asky28@gmail.com>
2020-07-15 17:54:28 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,9 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  def title_and_artist
+    self.artist.name + " - " + self.title
+  end
+  def artist_name
+      self.artist.name
+    end
 end
